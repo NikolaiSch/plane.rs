e8662c910ae951914da0c10b7ab0922c4d8c1bb7
Nikolai <vii@nikbook.lan>
2023-05-27 11:36:32 +0100

    rewrote request.rs

--- src/request.rs
+++ src/request.rs
@@ -1,16 +1,23 @@
 use {
-    anyhow::Result,
+    anyhow::{
+        anyhow,
+        bail,
+        Result
+    },
     http::{
         request::Builder,
-        Method,
-        Request
+        HeaderName,
+        HeaderValue,
+        Request,
+        Version
     },
     std::{
         io::{
             BufRead,
             BufReader
         },
-        net::TcpStream
+        net::TcpStream,
+        str::FromStr
     }
 };
 
@@ -20,21 +27,73 @@ pub struct IncomingRequest {
 }
 
 impl IncomingRequest {
-    fn new(stream: TcpStream) -> Self {
+    pub fn new(stream: TcpStream) -> Self {
         Self {
             req:    Request::builder(),
             reader: BufReader::new(stream)
                 .lines()
-                .(predicate)
                 .map(|x| x.unwrap())
+                .take_while(|x| !x.is_empty())
+                .collect()
         }
     }
 
-    fn parse(&mut self) -> Result<Self> {
+    fn parse(&mut self) -> Result<&mut Self> {
+        if let Some((&first_line, rest)) = self.reader.split_first() {
+            let req = UTF8Request {
+                first_line,
+                rest: rest.iter().collect()
+            };
+
+            req.parse_first_line(self)?;
+            req.parse_headers(self)?;
+
+            Ok(self)
+        } else {
+            bail!("Malformed Request: Empty Request");
+        }
     }
 }
 
 struct UTF8Request {
     first_line: String,
     rest:       Vec<String>
+}
+
+impl UTF8Request {
+    pub fn parse_first_line(&self, req: &mut IncomingRequest) -> Result<()> {
+        for (i, x) in self.first_line.split(" ").enumerate() {
+            match i {
+                0 => Ok(req.req.method(x)),
+                1 => Ok(req.req.uri(x)),
+                2 => Ok(req.req.version(Version::http_11)),
+                _ => bail!("Malformed Request: Invalid First Line of Request")
+            };
+        }
+
+        Ok(())
+    }
+
+    pub fn parse_headers(&self, req: &mut IncomingRequest) -> Result<()> {
+        self.rest
+            .iter()
+            .map(|x| Self::parse_header(x)?)
+            .map(|(n, v)| req.req.header(n, v))
+            .collect();
+
+        Ok(())
+    }
+
+    fn parse_header(header: &str) -> Result<(HeaderName, HeaderValue)> {
+        let parts: Vec<&str> = header.split(":").collect();
+
+        if let Some((&f, &s)) = parts.split_first() {
+            let name = HeaderName::from_str(f)?;
+            let val = HeaderValue::from_str(s.get(0).unwrap())?;
+
+            return Ok((name, val));
+        } else {
+            bail!("Malformed Request: Incorrect Header Format")
+        }
+    }
 }
