132db150bb4842d9d097983876d33aeb7a1ca070
Nikolai <vii@nikbook.lan>
2023-05-28 09:59:40 +0100

    going to clippy.

--- src/body_parser.rs
+++ src/body_parser.rs
@@ -1,4 +1,3 @@
-
 use {
     crate::Response,
     tokio::{
@@ -12,34 +11,33 @@ pub trait ToHTTP {
     #[instrument(level = Level::DEBUG, name = "Writing the Response to the `OwnedWriteHalf`", skip_all )]
     async fn write_to_stream(&mut self, stream: &mut OwnedWriteHalf) -> anyhow::Result<()> {
         let text = self.get_text()?;
-        event!(Level::TRACE, "")
+        event!(Level::TRACE, "successfully called get_text()");
 
         for line in text {
-            info!(line = line);
+            event!(Level::DEBUG, line = line, i = 0);
 
             stream.writable().await?;
             stream.write_all(line.as_bytes()).await?;
         }
         Ok(())
     }
+    fn get_text(&mut self) -> anyhow::Result<Vec<String>>;
+}
 
+impl ToHTTP for Response<Vec<String>> {
     fn get_text(&mut self) -> anyhow::Result<Vec<String>> {
         let mut v = vec![];
         v.push(format!("{} {} {}", "HTTP/1.1", self.status(), "OK"));
 
-        let headers: Vec<_> = self
+        let mut headers: Vec<_> = self
             .headers()
             .into_iter()
             .map(|(name, value)| format!("{}: {}", name, value.to_str().unwrap()))
             .collect();
 
-        headers.append(&mut headers);
-        headers.append(&mut self);
+        v.append(&mut headers);
+        v.append(&mut self.body_mut().to_owned());
 
         Ok(v)
     }
-}
-
-impl ToHTTP for Response<()> {
-    
 }
--- src/init.rs
+++ src/init.rs
@@ -1,26 +1,13 @@
 use {
-    std::{
-        time,
-        time::Duration
-    },
     tracing::{
         dispatcher,
         Dispatch,
         *
     },
-    tracing_subscriber::FmtSubscriber,
-    tracing_timing::{
-        Builder,
-        Histogram,
-        TimingSubscriber
-    }
+    tracing_subscriber::FmtSubscriber
 };
 
 pub fn init() -> () {
-    main_displayer();
-}
-
-fn main_displayer() {
     let subscriber = FmtSubscriber::builder().pretty().with_level(true).finish();
 
     let dis = Dispatch::new(subscriber);
